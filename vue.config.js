cc0ce04e84f942359e59c47e8074496e2ded961b
Simon Van Braeckel <simonvanbraeckel@gmail.com>
2022-05-29 18:55:11 +0200

    fuck, this is hard

--- vue.config.js
+++ vue.config.js
@@ -2,21 +2,4 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-
-  pluginOptions: {
-    prerenderSpa: {
-      registry: undefined,
-      renderRoutes: ["/"],
-      useRenderEvent: true,
-      headless: true,
-      onlyProduction: true,
-      postProcess: route => {
-        // Defer scripts and tell Vue it's been server rendered to trigger hydration
-        route.html = route.html
-          .replace(/<script (.*?)>/g, '<script $1 defer>')
-          .replace('id="app"', 'id="app" data-server-rendered="true"');
-        return route;
-      }
-    },
-  },
 });
